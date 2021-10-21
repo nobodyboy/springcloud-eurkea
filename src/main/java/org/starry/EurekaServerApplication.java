5d27cbb9604bfff34e929fee637c0b401272590e
shaoyongxing <shaoyongxing@meituan.com>
2021-10-22 07:02:54 +0800

    初始化工程

---
+++ src/main/java/org/starry/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package org.starry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * https://www.cnblogs.com/jing99/p/11576133.html
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
